7e250d5c17cbf7678206f8c2efde2d8dd94b174e
thoma <toto.francill@gmail.com>
2025-02-17 19:36:30 +0100

    Ajout du package test et de la classe PlanningCollectionsTets (non implémenté)

---
+++ src/test/java/com/example/tp1_1/modele/PlanningCollectionsTest.java
@@ -0,0 +1,24 @@
+package com.example.tp1_1.modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanningCollectionsTest {
+
+    @Test
+    void ajout() {
+    }
+
+    @Test
+    void getReservations() {
+    }
+
+    @Test
+    void testGetReservations() {
+    }
+
+    @Test
+    void getReservationsSemaine() {
+    }
+}
